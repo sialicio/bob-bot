6a07943373f6e6936ee5719bbb096bed0719dd08
Aldo Monteiro <aldo@aldomonteiro.com.br>
2016-12-06 17:00:35 -0200

    Adicionado token FB

--- index.js
+++ index.js
@@ -19,12 +19,29 @@ app.get('/', function (req, res) {
 })
 
 // for Facebook verification
+/*
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
         res.send(req.query['hub.challenge'])
     }
     res.send('Error, wrong token')
 })
+*/
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAZAwQbY0G9IBAPGL5zFEqCTjwRKZA0x7msZAsESasZAlzuLYjXrZBmrBzxPY6fvzNy7i1WqOmuhn8fT5Iv7PlDTnXMrew8jVK6daz0ZBVjsRqsc2KHryZBNTPvdqsVprdwXC1wUd2KuCIROPJFsC69EnJwo7gBLq60cbGUDwJQZBEZARUb2cjy5O"
 
 // Spin up the server
 app.listen(app.get('port'), function() {
